a659509462e134bd188fa5b5accb69f6b617e91f
PRADDIP5 <praddip05@gmail.com>
2024-10-21 12:27:49 +0530

    Update Macros_Basic.cpp

--- Macros_Basic.cpp
+++ Macros_Basic.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 #define ABS(num) ((num)<(0)? (-num) : (num))
 #define MAX(a,b) ((a)>(b)? (a) : (b))
+#define MIN(a,b) ((a)<(b)? (a) : (b))
 
 int main()
 {
 	cout << ABS(-3) << endl;
 	cout << ABS(3) << endl;
-	cout << MAX(2,5) << endl;
         cout << MAX(2,3) << endl;
+	cout << MIN(2,3) << endl;
 }
