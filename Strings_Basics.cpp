5d66a50a2033cf18864fa9c800b61028c0ae6494
PRADDIP5 <praddip05@gmail.com>
2024-10-21 12:54:10 +0530

    Update Strings_Basics.cpp

--- Strings_Basics.cpp
+++ Strings_Basics.cpp
@@ -7,7 +7,7 @@ int main() {
     std::string cities[4] = {"New york", "los angles", "chicago", "New jersey"};
 
     // Loop through the array and find cities that start with "New"
-    cout << "Cities that start with 'N' are:" << endl;
+    cout << "Name of the cities that start with character 'N' are:" << endl;
     for(int i = 0; i < 4; i++) {
         if(cities[i].substr(0, 1) == "N") {
             cout << cities[i] << endl;
