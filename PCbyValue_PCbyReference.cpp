44b6efda7bd701b135ed0c743164b31549b8f349
PRADDIP5 <praddip05@gmail.com>
2024-10-21 14:32:30 +0530

    Update and rename CallbyValue_CallbyReference.cpp to PCbyValue_PCbyReference.cpp

--- PCbyValue_PCbyReference.cpp
+++ PCbyValue_PCbyReference.cpp
@@ -2,18 +2,18 @@
 #include <string>
 using namespace std;
 
-void fun(string data) { // call by value 
+void fun(string data) { 
     data = "fun_data";
 }
 
-// void fun(string *data) {  // call by reference
+// void fun(string *data) {  
 //     *data = "fun_data";
 // }
 
 int main() {
     string data = "Main_Data";
-    fun(data);
-    //fun(&data);
+    fun(data); // pass/call by value 
+    //fun(&data); // pass/call by reference
     cout << data << endl;
     
     return 0;
