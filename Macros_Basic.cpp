0ced32cca44c52b6aaa522c6b91b02d3237715a3
PRADDIP5 <praddip05@gmail.com>
2024-10-21 12:22:52 +0530

    Update Macros_Basic.cpp

--- Macros_Basic.cpp
+++ Macros_Basic.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
 using namespace std;
-#define ABS(N) ((N)<(0)? (-N) : (N))
+
+#define ABS(Num) ((Num)<(0)? (-Num) : (Num))
 #define MAX(a,b) ((a)>(b)? (a) : (b))
 
 int main()
@@ -9,6 +10,5 @@ int main()
 	cout << ABS(-3) << endl;
 	cout << ABS(3) << endl;
 	cout << MAX(2,5) << endl;
-  cout << MAX(2,3) << endl;
-
+        cout << MAX(2,3) << endl;
 }
