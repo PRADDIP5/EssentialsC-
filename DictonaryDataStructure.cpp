9e25a15a766cacb8551a96999e77d9afc3a8496b
PRADDIP5 <praddip05@gmail.com>
2024-10-22 18:07:24 +0530

    Update DictonaryDataStructure.cpp

--- DictonaryDataStructure.cpp
+++ DictonaryDataStructure.cpp
@@ -64,3 +64,90 @@ int main() {
 
     return 0;
 }
+
+// #include <iostream>
+
+// int main() {
+//     // Print the header
+//     std::cout << "Key : Value" << std::endl;
+
+//     // Print the key-value pairs
+//     for (int i = 1; i <= 4; ++i) {
+//         std::cout << "   " << i << " : D" << i << std::endl;
+//     }
+
+//     return 0;
+// }
+
+// #include <iostream>
+
+// template <typename Key, typename Value>
+// class Dictionary {
+// private:
+//     Key* keys;
+//     Value* values;
+//     size_t capacity;
+//     size_t size;
+
+// public:
+//     Dictionary(size_t cap) : capacity(cap), size(0) {
+//         keys = new Key[capacity];
+//         values = new Value[capacity];
+//     }
+
+//     ~Dictionary() {
+//         delete[] keys;
+//         delete[] values;
+//     }
+
+//     void add(const Key& key, const Value& value) {
+//         if (size < capacity) {
+//             keys[size] = key;
+//             values[size] = value;
+//             size++;
+//         } else {
+//             std::cerr << "Dictionary capacity exceeded!" << std::endl;
+//         }
+//     }
+
+//     Value operator[](const Key& key) const {
+//         for (size_t i = 0; i < size; i++) {
+//             if (keys[i] == key) {
+//                 return values[i];
+//             }
+//         }
+//         throw std::out_of_range("Key not found");
+//     }
+
+//     void dump() const {
+//         std::cout << "Key : Value\n";
+//         for (size_t i = 0; i < size; i++) {
+//             std::cout << "   " << keys[i] << " : " << values[i] << "\n";
+//         }
+//     }
+// };
+
+// int main() {
+//     Dictionary<int, std::string> deviceCodeMap(4);
+//     deviceCodeMap.add(1, "D1");
+//     deviceCodeMap.add(2, "D2");
+//     deviceCodeMap.add(3, "D3");
+//     deviceCodeMap.add(4, "D4");
+
+//     std::string deviceName = deviceCodeMap[2];
+//     std::cout << deviceName << std::endl; // Output: D2
+
+//     // Dump all the items from deviceCodeMap
+//     deviceCodeMap.dump();
+//     /*
+//      Output:
+//      Key : Value
+//         1 : D1
+//         2 : D2
+//         3 : D3
+//         4 : D4
+//     */
+
+//     return 0;
+// }
+
