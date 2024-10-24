d6e6a580996df3579d09680276319d86f10f4d3b
PRADDIP5 <praddip05@gmail.com>
2024-10-24 15:08:04 +0530

    Update SpeedCalc_CloudConnect.cpp

--- SpeedCalc_CloudConnect.cpp
+++ SpeedCalc_CloudConnect.cpp
@@ -3,112 +3,97 @@
 #include <string>
 using namespace std;
 
-class IntrFCommunication {
+class ICloudCommunicator {
 public:
     virtual int pushMessage(string message) = 0;
 };
 
-class IOTCloudCommunicator: public IntrFCommunication{
+class IOTCloudCommunicator: public ICloudCommunicator{
     public:
           int  pushMessage(string message){
-              //Interact with internet and push the message to MessageQueue
-            //if communiction succesfull return value from ramge {200 -400} status code otherwise returns errocode (400 - 500)
-              std::random_device rd;
-                std::mt19937 gen(rd());
-                std::uniform_int_distribution<> distr(200, 500);
-                return distr(gen);
             
+            //Interact with internet and push the message to MessageQueue
+            //if communiction succesfull return value from ramge {200 -400} status code otherwise returns errocode (400 - 500)
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_int_distribution<> distr(200, 500);
+            return distr(gen);
           }
+
 };
 
-class IntrFSpeedSensor {
+class ISpeedSensor{
 public:
     virtual int getCurrentSpeed() = 0;
 };
 
-class BNFSpeedSensor: public IntrFSpeedSensor{
+class BNFSpeedSensor: public ISpeedSensor{
         public:
-            int   getCurrentSpeed(){
-                std::random_device rd;
-                std::mt19937 gen(rd());
-                std::uniform_int_distribution<> distr(1, 100);
-                return distr(gen);
+            int  getCurrentSpeed(){
+                    
+                    std::random_device rd;
+                    std::mt19937 gen(rd());
+                    std::uniform_int_distribution<> distr(1, 100);
+                    return distr(gen);
             }
 };
 
+class IMessageLogger{
+    public:
+    virtual void write(const string& message)=0;
+    virtual ~IMessageLogger(){}
+};
+class TerminalLogger:public IMessageLogger{
+    public:
+    void write(const string& message){
+        cout<<message<<endl;
+    }
+};
 class SpeedMonitor{
-  private : int _speedThreshold;
-  IntrFSpeedSensor* _speedSensor;
-  IntrFCommunication* _Communicator;
+    private : 
+        int _speedThreshold;
+        ISpeedSensor* _speedSensorInstance;
+        ICloudCommunicator* _cloudCommuniccator;
+        IMessageLogger* _logger;
+  
       public:
-SpeedMonitor(int speedThreshold, IntrFSpeedSensor* speedSensor, IntrFCommunication* Communicator): _speedThreshold{speedThreshold}, _speedSensor(speedSensor),  _Communicator(Communicator){}
+      
+          SpeedMonitor(ISpeedSensor* sensor, ICloudCommunicator* communicator,IMessageLogger* logger, int speedThreshold): 
+          _speedThreshold{speedThreshold}, _speedSensorInstance {sensor}, _cloudCommuniccator{communicator},
+          _logger{logger}{}
 
           void monitor(){
+
             if(_speedThreshold  < 1 || _speedThreshold > 100){
-              cout<<"_speedThreshold value must be in the range {1-100} "<<_speedThreshold<<endl;
+        this->_logger->write("_speedThreshold value must be in the ramge {1-100}" + to_string(_speedThreshold));
+        return;
             }
-              
-         //   BNFSpeedSensor speedSensorInstance;
-         //   IOTCloudCommunicator cloudCommuniccator;
-            int currentSpeedInKmph=_speedSensor->getCurrentSpeed();
-            cout<<"Current Speed In Kmph "<<currentSpeedInKmph<<endl;
-            
-            if(currentSpeedInKmph > _speedThreshold){
-                  double mph = currentSpeedInKmph * 0.621371;
-                  string message="Current Speed in Miles "+ to_string(mph);
-                 int statusCode= _Communicator->pushMessage(message);
-                  if(statusCode > 400){
+
+            int currentSpeedInKmph= _speedSensorInstance->getCurrentSpeed();
+                    if(currentSpeedInKmph > _speedThreshold){
+                
+                double mph = currentSpeedInKmph * 0.621371;
+                string message="Current Speed in Miles "+ to_string(mph);
+                int statusCode= _cloudCommuniccator->pushMessage(message);
+
+                if(statusCode > 400){
+
                       //Log Message to Console
-                       cout<<"Error In Communication Unable to Contact Server "<<message<< endl;
+                       this->_logger->write("Error In Communication Unable to Contact Server " + message);
                   }
-                 
-            }
+            }  
           }
 };
 
-class MockSpeedSensor : public IntrFSpeedSensor {
-public:
-    int speed; 
-    MockSpeedSensor(int spd) : speed(spd) {}
-
-    int getCurrentSpeed() {
-        return speed; // Return the predefined speed
-    }
-};
-
-lass MockCloudCommunicator : public IntrFCommunication {
-public:
-    int statusMsg; 
-
-    MockCloudCommunicator(int Msg) : statusMsg(Msg) {}
-
-    int pushMessage(string message) {
-        return statusMsg; 
-    }
-};
-
-void testSpeedMonitor(int speed, int statusMsg, bool expectError) {
-    MockSpeedSensor mockSensor(speed);
-    MockCloudCommunicator mockCommunicator(statusMsg);
-    SpeedMonitor monitor(10, &mockSensor, &mockCommunicator);
-}
-
-// int main(){
-// BNFSpeedSensor speedSensor;
-// IOTCloudCommunicator Communicator;
-// SpeedMonitor instance {10, &speedSensor, &Communicator};
-//   instance.monitor();
-//   instance.monitor();
-//   instance.monitor();
-//   instance.monitor();
-//   instance.monitor();
-  
-//   return 0;
-// }
-
-int main() {
+int main(){
     
-    testSpeedMonitor(20, 200, false); 
-    
-    return 0;
+    BNFSpeedSensor speedSensor;
+    IOTCloudCommunicator cloudCommunicator;
+    TerminalLogger logger;
+    SpeedMonitor instance(&speedSensor, &cloudCommunicator, &logger,150);
+    instance.monitor();
+    instance.monitor();
+    instance.monitor();
+    instance.monitor();
+    instance.monitor();
 }
