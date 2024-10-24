8ba12f944553d1e9bb30dc2ca89baed90d9f3d11
PRADDIP5 <praddip05@gmail.com>
2024-10-24 10:55:55 +0530

    Update DepInjPrinciple.cpp

--- DepInjPrinciple.cpp
+++ DepInjPrinciple.cpp
@@ -3,38 +3,38 @@ using namespace std;
 
 class IICEngine{
 	public:
-			//pure virtual method;
-			virtual void crank()=0;
-			virtual void halt()=0;
+		//pure virtual method;
+		virtual void crank()=0;
+		virtual void halt()=0;
 };
 
 //Low Level Module, Service
 //VericoreEngine implemets contract i.e IICEngine
 class VericoreEngine:public IICEngine{
 	public:
-		void crank(){ cout << "Engined cranked" << endl; }
-		void halt(){  cout << "Engined halted" << endl; }
+	void crank(){ cout << "Engined cranked" << endl; }
+	void halt(){  cout << "Engined halted" << endl; }
 
 };
 
 //High Level Module , consumer,client
 class TataCar{
-	IICEngine* _engine;
+IICEngine* _engine;
 	public:
-		TataCar(IICEngine* engine): _engine{engine}{}
-		void start(){
-			_engine->crank();
-		}	
-		void stop(){
-			_engine->halt();
-		}
+	TataCar(IICEngine* engine): _engine{engine}{}
+	void start(){
+		_engine->crank();
+	}	
+	void stop(){
+		_engine->halt();
+	}
 };
 
 int main(){
-		VericoreEngine _vericoreEngine;
-		TataCar _hexaIstance {&_vericoreEngine};
-		_hexaIstance.start();
-		_hexaIstance.stop();
+	VericoreEngine _vericoreEngine;
+	TataCar _hexaIstance {&_vericoreEngine};
+	_hexaIstance.start();
+	_hexaIstance.stop();
 
 	return 0;
 }
